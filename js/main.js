8956fc3c5b9fcda0505eb5728c0517cf2e4ef2ed
Yuhei Kubo <y.kubo@kc-factory.co.jp>
2020-03-05 16:41:07 +0900

    main.js変更/合計値表示

--- js/main.js
+++ js/main.js
@@ -1,22 +1,10 @@
-  //main.jsではメインの処理だけ書く
-  /*
-  メインはcsvのデータを取得する
-  取得したCSVをhtml上にinnerHTMLする
-  以上！！
-  */
-
-  /* めいんじゃない機能は切り離す
-  for文を縮められないか？
-  データをどんどん順次処理的にパスするのではなく
-  必要なところに関数をつまんでくるようにできないか？？
-  
-  */
-
 
   //index.htmlからcsv出力先の要素を取得
   const outputElement = document.getElementById('output_csv');
+  
 
-  //todouhuken2.csvを取得し、内容をoutput_csvの要素内に記載
+  //getCsvData()でtodouhuken2.csvを取得
+  //csvを定数responseに代入しconvertArray(data)に飛ばす
   function getCsvData(dataPath) {
     const request = new XMLHttpRequest();
     request.addEventListener('load', (event) => {
@@ -27,15 +15,16 @@
     request.send();
    }
 
-  //  dataStringにsplitで改行毎に分割し一次元配列とする
-  //　dataArrayの各行を「,」で分割し一次元配列化する
+
+  //引数data内のcsvを二次元配列っぽく配列化
+  //その後<tr><td>を充てindex.htmlに出力
+  //dataArray(二次元配列csv)をgetSum(twoDimeArray)へ飛ばす
   function convertArray(data) {
     const dataArray = [];
     const dataString = data.split('\n');
-    for(var i = 0; i < dataString.length; i++) {
+    for(var i = 0; i < dataString.length - 1; i++) {
       dataArray[i] = dataString[i].split(',');
     }
-    //空文字列insertElementにdataArrayにテーブルタグを足している
     var insertElement = '';
     dataArray.forEach(function(element) {
       insertElement += '<tr>';
@@ -45,33 +34,25 @@
       insertElement += '</tr>';
     });
      outputElement.innerHTML = insertElement;
-  }
-   getCsvData('../population.csv');
+    //  getSum(dataArray);
 
-  //まずは破壊的メソッドshiftを変更
-  //非破壊的メソッドでDupulicateを作る
-   function getIntArray(twoDimeArray) {
-    // twoDimeArray.shift();
-    const intArray = [];
-    for(var i = 1; i < twoDimeArray.length; i++) {
-      // twoDimeArray[i].shift();
-      intArray[i] = [];
-      for(var j = 1; j < twoDimeArray[i].length; j++) {
-        intArray[i] = twoDimeArray[i].map(Number);
+    getSum(dataArray);
+  }
+
+
+  function getSum(dataArray) {
+    const intArray = dataArray.concat();
+    for(var i = 1; i < intArray.length; i++) {
+      for(var j = 1; j < intArray[i].length; j++) {
+        intArray[i][j] = parseInt(dataArray[i][j], 10);
       }
     }
-    console.log(intArray);
-    // getSum(intArray);
-  }
 
-  //とりあえずfor文を短くする
-  //intArrayがつながっていきているやつを切り離す
-  function getSum(intArray) {
     var rowSum;
     const rowSumArray = [];
-    for(var i = 0; i < intArray.length; i++){
+    for(var i = 1; i < intArray.length; i++){
       rowSum = 0;
-      for(var j = 0; j < intArray[i].length; j++){
+      for(var j = 1; j < intArray[i].length; j++){
         rowSum += intArray[i][j];
       }
       rowSumArray.push(rowSum);
@@ -79,44 +60,37 @@
 
     var colomnSum;
     const colomnSumArray = [];
-    for(var i = 0; i < intArray[i].length; i++){
+    for(var i = 1; i < intArray[i].length; i++){
        colomnSum = 0;
-       for(var j = 0; j < intArray.length; j++){
+       for(var j = 1; j < intArray.length; j++){
          colomnSum += intArray[j][i];
        }
        colomnSumArray.push(colomnSum);
     }
-    insertRow(colomnSumArray,'年別合計');
-  }
-
-
-  var insertRow = function insert(colomnSumArray, text) {
-    // console.log(colomnSumArray);
-    // const insertArray = [];
-    insertArray = colomnSumArray;
-    // // insertArray[0][0] = text;
-    insertArray[0] = text;
-    // // console.log(insertArray);
-    newRow = outputElement.insertRow(-1);
-    insertElement = '';
-    // newTd = document.createElement('td');
-    insertArray.forEach(function(element){
-      insertElement += '<td>' + '</td>';
+    var lastColomnSum = 0;
+    for(var i = 0; i < colomnSumArray.length; i++){
+      lastColomnSum += colomnSumArray[i];
+    }
+    colomnSumArray.unshift('年別合計');
+    var newRow = outputElement.insertRow();
+    colomnSumArray.forEach(function(element){
+      var newCell = newRow.insertCell();
+      var newTexts = document.createTextNode(element);
+      newCell.appendChild(newTexts);
     });
-    newRow[0].appendChild(insertElement);
-    // // console.log(insertArray);
-    // // var newTr = document.createElement('tr');
-    // // var newTd = 
 
+    rowSumArray.unshift('各都市合計');
+    for(var i = 0; i < rowSumArray.length; i++){
+      var newColomn = outputElement.rows[i].insertCell(-1);
+      var newTexts = document.createTextNode(rowSumArray[i]);
+      newColomn.appendChild(newTexts);
+    }
 
+    var lastInsert = document.createTextNode(lastColomnSum);
+    var lastCell = outputElement.rows[48].insertCell(-1);
+    lastCell.appendChild(lastInsert);
 
-    // var insertElement = '';
-    // insertArray.forEach(function(element) {
-    //   insertElement += '<tr>';
-    //   element.forEach(function(childElement) {
-    //     insertElement += '<td>' + childElement + '</td>';
-    //   });
-    //   insertElement += '</tr>';
-    // });
-    // outputElement.appendChild(insertElement);
   }
+
+  getCsvData('../population.csv');
+
